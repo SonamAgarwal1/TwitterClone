9bd24308aaa33292ad1818410fbaa4c66217f905
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-06-03 19:31:03 +0530

    User Model

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
