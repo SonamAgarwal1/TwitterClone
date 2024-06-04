aadba76b1b3a1e6ace9a44b60b163b0197464c9c
SonamAgarwal1 <agarwal.sonam12@gmail.com>
2024-06-04 22:54:14 +0530

    Auth sign up controller part-1

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,21 @@
 export const signup = async (req, res) => {
-  res.json({ data: "Hit the sign up URL" });
+  const { username, fullName, email, password } = req.body;
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).json({ error: "Username is already taken" });
+  }
+
+  const existingEmail = await User.findOne({ email });
+  if (existingEmail) {
+    return res.status(400).json({ error: "Email is already taken" });
+  }
+  res.json({
+    data: "Hit the sign up URL",
+  });
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); //to parse req body
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
